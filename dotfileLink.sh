ea62ee0ca384628d7c5a5f52ee1b3507728d8de5
SENOO Ken <mslamsh20131029@outlook.jp>
2014-11-17 10:28:38 +0900

    Windowsでは.atomを$USERPROFILEにリンクを貼るように変更

--- dotfileLink.sh
+++ dotfileLink.sh
@@ -1,13 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 # @author: SENOO, Ken
-# (Last Update: 2014-05-06T22:27+09:00)
+# (Last Update: 2014-11-17T10:27+09:00)
 
 script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 
 for dotfile in ${script_dir}/.??*
-do 
-    if [ $dotfile != '.git' ]
-    then
-        ln -sfd  $dotfile  ~/
-    fi
+do
+  if [ $OS == "Windows_NT" ]; then
+    case $dotfile in
+      *.atom*) ln -sfd $dotfile $USERPROFILE/
+    esac
+  fi
+  ln -sfd  $dotfile  ~/
 done
