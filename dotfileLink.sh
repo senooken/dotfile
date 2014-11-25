b598395cec5f3ac43b6a3c298331b2b28c36f43d
SENOO Ken <mslamsh20131029@outlook.jp>
2014-11-25 13:35:15 +0900

    比較演算子を修正

--- dotfileLink.sh
+++ dotfileLink.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # @author: SENOO, Ken
-# (Last Update: 2014-11-23T19:14+09:00)
+# (Last Update: 2014-11-25T13:35+09:00)
 
 script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 
 for dotfile in ${script_dir}/.??*
 do
-  if [ $OS == "Windows_NT" ] && echo $dotfile | grep -q atom ; then
+  if [ "$OS" == "Windows_NT" ] && echo $dotfile | grep -q atom ; then
     ln -sfd $dotfile $USERPROFILE/
   else
     ln -sfd  $dotfile  ~/
