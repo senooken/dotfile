ebdb8ec8c790b04ae71491b8d96912535c9a5d6d
SENOO Ken <mslamsh20131029@outlook.jp>
2014-12-17 00:23:02 +0900

    recover

--- dotfileLink.sh
+++ dotfileLink.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 # @author: SENOO, Ken
-# (Last Update: 2014-11-25T13:35+09:00)
+# (Last Update: 2014-12-16T16:28+09:00)
 
 script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 
-for dotfile in ${script_dir}/.??*
+windir=".atom tecplot.cfg"
+homefile="tecplot.cfg"
+for dotfile in ${script_dir}/{.??*,$homefile}
 do
-  if [ "$OS" == "Windows_NT" ] && echo $dotfile | grep -q atom ; then
-    ln -sfd $dotfile $USERPROFILE/
+  if [ "$OS" == "Windows_NT" ]; then
+    for winconf in $windir
+    do
+      if echo $dotfile | grep -q $winconf; then
+        ln -sfd $dotfile $USERPROFILE/
+      fi
+    done
   else
     ln -sfd  $dotfile  ~/
   fi
