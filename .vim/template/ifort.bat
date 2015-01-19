67ccd69ed8e5b3d685f92d1fe10fd5301797ae89
SENOO Ken <mslamsh20131029@outlook.jp>
2015-01-19 10:34:02 +0900

    ifort.batでヘッダーの重複を除去。bat.tmpl：ライセンスを明記

--- .vim/template/ifort.bat
+++ .vim/template/ifort.bat
@@ -1,7 +1,3 @@
-: (File name: ifort.bat)
-: Author: SENOO, Ken
-: License: MIT
-
 @echo off
 call "C:\Program Files (x86)\Intel\Compiler\11.1\054\bin\ia32\ifortvars_ia32.bat"
 ifort.exe /nologo *.f??
