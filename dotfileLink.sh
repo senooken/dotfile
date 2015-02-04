ca6f80ee0f8a0c67176a46bfd2b6562754d825a2
SENOO Ken <mslamsh20131029@outlook.jp>
2015-02-04 16:27:13 +0900

    add comment

--- dotfileLink.sh
+++ dotfileLink.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 # @author: SENOO, Ken
-# (Last Update: 2015-01-19T09:51+09:00)
+# (Last Update: 2015-02-04T16:26+09:00)
 
 script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 
 windir=".atom tecplot.cfg"
 homefile="tecplot.cfg"
+# .で始まるファイルとhomefileで指定したファイルを対象
+# windirで指定したファイルは$USERPROFILEにリンク
 for dotfile in ${script_dir}/{.??*,$homefile}
 do
   if [ "$OS" == "Windows_NT" ]; then
