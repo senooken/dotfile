0ef074b4b756ae748fef295ec7b4496b47baa22d
SENOO Ken <mslamsh20131029@outlook.jp>
2015-10-29 21:18:23 +0900

    dotfilelinkを改善。.mozcをdotfileに移動

--- dotfileLink.sh
+++ dotfileLink.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # \file dotfileLink.sh
 # \author SENOO, Ken
-# \date 2015-09-28T17:15+09:00
 
 script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 
@@ -9,11 +8,24 @@ windir=".atom tecplot.cfg"
 homefile="tecplot.cfg"
 # .で始まるファイルとhomefileで指定したファイルを対象
 # windirで指定したファイルは$USERPROFILEにリンク
-for dotfile in ${script_dir}/{.??*,$homefile}
-do
-  if [ "$OS" == "Windows_NT" ] && echo $windir | grep -q ${dotfile##*/}; then
-        ln -sfd $dotfile $USERPROFILE/
-  else
-    ln -sfd  $dotfile  ~/
+for dotfile in ${script_dir}/{.??*,$homefile}; do
+  if [ "${OS}" != "Windows_NT" ]; then
+    ln -sfd  "${dotfile}"  ~/
+  elif [[ .*${windir} =~ ${dotfile##*/} ]]; then
+    ln -sfd "$dotfile" "${USERPROFILE}/"
+  elif [[ "${dotfile}" =~ .*mozc$ ]]; then
+    ln -sfd "${dotfile}" "${USERPROFILE}/LocalLow/Google/Google Japanese Input"
   fi
 done
+
+if [ "$OS" == "Windows_NT" ]; then
+  for dotfile in ${script_dir}/windows/*; do
+    ln -sfd "${dotfile}" "${APPDATA}/"
+  done
+fi
+
+if [ "$OS" != "Windows_NT" ]; then
+  for dotfile in ${script_dir}/linux/.??*; do
+    ln -sfd "${dotfile}" ~/
+  done
+fi
