7169d9c5f2676c4544f240df55da140db89c90f5
SENOO Ken <mslamsh20131029@outlook.jp>
2015-12-02 15:25:05 +0900

    add ignore list to FLIPGEN

--- install-Chocolatey.bat
+++ install-Chocolatey.bat
@@ -2,22 +2,21 @@
 :: \author SENOO, Ken
 :: \copyright CC0
 
+:: TODO: Ç±Ç±ÇÇøÇ·ÇÒÇ∆ìÆçÏÇ∑ÇÈÇÊÇ§Ç…Ç∑ÇÈÅB
+
 PKG="
 keepass
 fastcopy
 chromium
 thunderbird
 libreoffice
-adobereader
 foxitreader
 GoogleJapaneseInput
 LinkShellExtension
 winmerge-jp
 qttabbar
 FoxitReader
-adobereader
 "
 
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
-upgrade chocolatey
 choco install -y %PKG%
