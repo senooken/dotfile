ef12861f698806266766e0363a598f6e157c56bf
SENOO Ken <mslamsh20131029@outlook.jp>
2015-12-04 16:56:09 +0900

    fix PKG variable for line break

--- install-Chocolatey.bat
+++ install-Chocolatey.bat
@@ -2,21 +2,21 @@
 :: \author SENOO, Ken
 :: \copyright CC0
 
-:: TODO: Ç±Ç±ÇÇøÇ·ÇÒÇ∆ìÆçÏÇ∑ÇÈÇÊÇ§Ç…Ç∑ÇÈÅB
-
-PKG="
-keepass
-fastcopy
-chromium
-thunderbird
-libreoffice
-foxitreader
-GoogleJapaneseInput
-LinkShellExtension
-winmerge-jp
-qttabbar
-FoxitReader
-"
+PKG=^
+ keepass^
+ chromium^
+ thunderbird^
+ GoogleJapaneseInput^
+ LinkShellExtension^
+ qttabbar^
+ FoxitReader^
 
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 choco install -y %PKG%
+
+PKG=^
+ fastcopy^
+ LibreCAD^
+ BlueGriffon^
+ VLC^
+ libreoffice^
