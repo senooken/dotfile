7f04f01abfefe8c66560c3544257d158d0afe40b
SENOO Ken <mslamsh20131029@outlook.jp>
2016-04-17 16:31:05 +0900

    句読点を点に変更

--- specific/.mozc/relink_mozc.bat
+++ specific/.mozc/relink_mozc.bat
@@ -2,13 +2,13 @@ REM \file      relink_mozc.bat
 REM \author    SENOO, Ken
 REM \copyright CC0
 REM \date      first created date: 2016-04-17T12:58+09:00
-REM \date      last  updated date: 2016-04-17T14:46+09:00
-REM \brief     WindowsのGoogle日本語入力のIME設定を更新後、共有ディレクトリに更新された設定ファイルをコピーしてリンクを貼り直す。
+REM \date      last  updated date: 2016-04-17T16:30+09:00
+REM \brief     Google日本語入力のIME設定を更新後，共有ディレクトリに更新された設定ファイルをコピーしてリンクを貼り直す。
 REM 使い方
 REM 共有したいファイルを FILE 変数に空白区切りで記入する。
 REM Dropboxなどで共有しているディレクトリにこのバッチファイルを配置して実行する。
-REM 共有ディレクトリと同じ場所にこのバッチファイルを配置したくなければ、SHARE_DIR 変数に共有ディレクトリの場所を指定する。
-REM 管理者権限が不要で済むように、シンボリックではなくハードリンクを使っている。
+REM 共有ディレクトリと同じ場所にこのバッチファイルを配置したくなければ，SHARE_DIR 変数に共有ディレクトリの場所を指定する。
+REM 管理者権限が不要で済むように，シンボリックではなくハードリンクを使っている。
 
 SETLOCAL
 SET FILE=config1.db user_dictionary.db
