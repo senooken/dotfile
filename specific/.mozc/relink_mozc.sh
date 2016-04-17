35744f05201c653fdd44e64914ebb4606d1b3ecd
SENOO Ken <mslamsh20131029@outlook.jp>
2016-04-18 00:13:19 +0900

    シェルスクリプトから未定義変数で警告を出すオプションを削除

--- specific/.mozc/relink_mozc.sh
+++ specific/.mozc/relink_mozc.sh
@@ -3,14 +3,12 @@
 # \author    SENOO, Ken
 # \copyright CC0
 # \date      first created date: 2016-04-17T14:35+09:00
-# \date      last  updated date: 2016-04-17T16:30+09:00
+# \date      last  updated date: 2016-04-18T00:12+09:00
 # \brief     mozcのIME設定を更新後，共有ディレクトリに更新された設定ファイルをコピーしてリンクを貼り直す。
 # 共有したいファイルを FILE 変数に空白区切りで記入する。
 # Dropboxなどで共有しているディレクトリにこのスクリプトを配置して実行する。
 # 共有ディレクトリと同じ場所にこのスクリプトを配置したくなければ，SHARE_DIR 変数に共有ディレクトリの場所を指定する
 # CygwinなどWindowsでの動作も考慮して，管理者権限が不要なハードリンクを使用。
-
-set -u
 
 FILE="config1.db user_dictionary.db"
 OS_DIR=~/.mozc
