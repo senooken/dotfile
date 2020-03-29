c0672b13f560970fad5877e00cfe3d6b2e72a84b
SENOO, Ken <contact@senooken.jp>
2020-03-29 11:57:12 +0900

    Fix script constructure

--- .vim/template/template.sh
+++ .vim/template/template.sh
@@ -8,17 +8,26 @@
 ## \date      Updated: 
 ################################################################################
 
-## \brief Initialize POSIX shell environment
-init() {
-	PATH="$(command -p getconf PATH 2>&-):${PATH:-.}"
-	export PATH="${PATH#:}" LC_ALL='C'
-	umask 0022
-	set -eu
+EXE_NAME='template'
+
+template() {
+	
 }
 
-is_main() (
-	EXE_NAME='.sh'
-	[ "$EXE_NAME" = "${0##*/}" ]
-)
+main() {
+	## \brief Initialize POSIX shell environment
+	init() {
+		PATH="$(command -p getconf PATH 2>&-):${PATH:-.}"
+		export PATH="${PATH#:}" LC_ALL='C'
+		umask 0022
+		set -eu
+	}
 
-init
+	is_main() { [ "$EXE_NAME.sh" = "${0##*/}" ]; }
+	if is_main; then
+		init
+		$EXE_NAME ${1+"$@"}
+	fi
+}
+
+main ${1+"$@"}
